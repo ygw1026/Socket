10d142a45c73382cd90f160b0799c5a948c5dbe0
ygw1026 <ygw1026@naver.com>
2024-10-07 17:56:08 +0900

    step02 ResponseFactoryTest 누락으로 내용 추가함

--- src/test/java/com/nhnacademy/server/method/response/ResponseFactoryTest.java
+++ src/test/java/com/nhnacademy/server/method/response/ResponseFactoryTest.java
@@ -1,5 +1,25 @@
 package com.nhnacademy.server.method.response;
 
-public class ResponseFactoryTest {
-    
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.nhnacademy.server.method.response.exception.ResponseNotFoundException;
+import com.nhnacademy.server.method.response.impl.EchoResponse;
+
+class ResponseFactoryTest {
+     @Test
+    @DisplayName("getResponse By method")
+    void getResponse() {
+        Response response = ResponseFactory.getResponse("echo");
+        Assertions.assertInstanceOf(EchoResponse.class, response);
+    }
+
+    @Test
+    @DisplayName("getResponse by something")
+    void getResponseByNotExistMethodName(){
+        Assertions.assertThrows(ResponseNotFoundException.class,()->{
+            ResponseFactory.getResponse("something");
+        });
+    }
 }
